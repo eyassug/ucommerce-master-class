20f3add246c613975f0e9f68951138301892b159
S?ren Spelling Lund <S?ren Spelling Lund@localhost>
2013-05-06 21:16:35 +0200

    Added macroscripts from Birmingham training

---
+++ src/MyUCommerceApp/MyOrderContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCommerce.EntitiesV2;
+using UCommerce.Runtime;
+using UCommerce.Security;
+
+namespace MyUCommerceApp.Library
+{
+	public class MyOrderContext : OrderContext
+	{
+		private readonly IMemberService _memberService;
+		private readonly IRepository<PurchaseOrder> _purchaseOrderRepo;
+
+		public MyOrderContext(ICatalogContext catalogContext, IMemberService memberService, IRepository<PurchaseOrder> purchaseOrderRepo) : base(catalogContext)
+		{
+			_memberService = memberService;
+			_purchaseOrderRepo = purchaseOrderRepo;
+		}
+
+		public override UCommerce.EntitiesV2.Basket GetBasket()
+		{
+			var basket = base.GetBasket(false);
+
+			if (basket != null)
+				return basket;
+
+			if (_memberService.IsLoggedIn())
+			{
+				var member = _memberService.GetCurrentMember();
+				// return _purchaseOrderRepo.Select().Where(x => x.MemberId == member.MemberId && x.OrderStatus.OrderStatusId == (int)OrderStatus.Basket).SingleOrDefault();
+				
+			}
+
+			return null;
+		}
+	}
+}
