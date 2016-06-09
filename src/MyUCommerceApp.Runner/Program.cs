86de7062d2d8eb1471d691f9b447834e3a10ee10
MSK <MSK@localhost>
2016-06-09 09:54:29 +0200

    Missing files in solution.

---
+++ src/MyUCommerceApp.Runner/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using MyUCommerceApp.BusinessLogic.Queries;
+using UCommerce.EntitiesV2;
+using UCommerce.Infrastructure;
+
+namespace MyUCommerceApp.Integration
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var order = ObjectFactory
+				.Instance
+				.Resolve<IRepository<PurchaseOrder>>()
+				.Select(new LatestOrderQuery()).First();
+		}
+	}
+}
