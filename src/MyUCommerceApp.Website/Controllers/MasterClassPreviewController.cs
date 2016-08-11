c07d6aad249f2c9c78697f661a55679cf3f15540
MSK <MSK@localhost>
2016-08-11 14:44:56 +0200

    Fixed checkout.

--- src/MyUCommerceApp.Website/Controllers/MasterClassPreviewController.cs
+++ src/MyUCommerceApp.Website/Controllers/MasterClassPreviewController.cs
@@ -5,54 +5,80 @@ using System.Web;
 using System.Web.Mvc;
 using UCommerce.Api;
 using UCommerce.EntitiesV2;
+using UCommerce;
 using MyUCommerceApp.Website.Models;
 
 namespace MyUCommerceApp.Website.Controllers
 {
 	public class MasterClassPreviewController : Umbraco.Web.Mvc.RenderMvcController
     {
-		public ActionResult Index()
-		{
-			PurchaseOrderViewModel model = MapOrder();
-			
-			model.BillingAddress = MapOrderAddress(TransactionLibrary.GetBillingInformation());
-			model.ShippingAddress = MapOrderAddress(TransactionLibrary.GetShippingInformation());
+        public ActionResult Index()
+        {
+            PurchaseOrderViewModel model = MapOrder();
 
-			return View("/Views/preview.cshtml", model);
-		}
+            model.BillingAddress = MapOrderAddress(TransactionLibrary.GetBillingInformation());
+            model.ShippingAddress = MapOrderAddress(TransactionLibrary.GetShippingInformation());
 
-		private AddressViewModel MapOrderAddress(OrderAddress orderAddress)
-		{
-			var addressDetails = new AddressViewModel();
+            return View("/Views/preview.cshtml", model);
+        }
 
-			addressDetails.FirstName = orderAddress.FirstName;
-			addressDetails.LastName = orderAddress.LastName;
-			addressDetails.EmailAddress = orderAddress.EmailAddress;
-			addressDetails.PhoneNumber = orderAddress.PhoneNumber;
-			addressDetails.MobilePhoneNumber = orderAddress.MobilePhoneNumber;
-			addressDetails.Line1 = orderAddress.Line1;
-			addressDetails.Line2 = orderAddress.Line2;
-			addressDetails.PostalCode = orderAddress.PostalCode;
-			addressDetails.City = orderAddress.City;
-			addressDetails.State = orderAddress.State;
-			addressDetails.Attention = orderAddress.Attention;
-			addressDetails.CompanyName = orderAddress.CompanyName;
-			addressDetails.CountryId = orderAddress.Country != null ? orderAddress.Country.CountryId : -1;
+        private AddressViewModel MapOrderAddress(OrderAddress orderAddress)
+        {
+            var addressDetails = new AddressViewModel();
 
-			return addressDetails;
-		}
+            addressDetails.FirstName = orderAddress.FirstName;
+            addressDetails.LastName = orderAddress.LastName;
+            addressDetails.EmailAddress = orderAddress.EmailAddress;
+            addressDetails.PhoneNumber = orderAddress.PhoneNumber;
+            addressDetails.MobilePhoneNumber = orderAddress.MobilePhoneNumber;
+            addressDetails.Line1 = orderAddress.Line1;
+            addressDetails.Line2 = orderAddress.Line2;
+            addressDetails.PostalCode = orderAddress.PostalCode;
+            addressDetails.City = orderAddress.City;
+            addressDetails.State = orderAddress.State;
+            addressDetails.Attention = orderAddress.Attention;
+            addressDetails.CompanyName = orderAddress.CompanyName;
+            addressDetails.CountryId = orderAddress.Country != null ? orderAddress.Country.CountryId : -1;
 
-		private PurchaseOrderViewModel MapOrder()
-		{
-			var basketModel = new PurchaseOrderViewModel();
+            return addressDetails;
+        }
 
-			return basketModel;
-		}
-		
-		[HttpPost]
-		public ActionResult Post()
-		{
-			return View("/Views/Complete.cshtml");
-		}
-	}
+        private PurchaseOrderViewModel MapOrder()
+        {
+            PurchaseOrder basket = TransactionLibrary.GetBasket(false).PurchaseOrder;
+
+            var basketModel = new PurchaseOrderViewModel();
+
+            foreach (var orderLine in basket.OrderLines)
+            {
+                var orderLineModel = new OrderlineViewModel();
+                orderLineModel.ProductName = orderLine.ProductName;
+                orderLineModel.Sku = orderLine.Sku;
+                orderLineModel.VariantSku = orderLine.VariantSku;
+                orderLineModel.Total =
+                    new Money(orderLine.Total.GetValueOrDefault(), orderLine.PurchaseOrder.BillingCurrency).ToString();
+
+                orderLineModel.Quantity = orderLine.Quantity;
+
+                basketModel.OrderLines.Add(orderLineModel);
+            }
+
+            basketModel.DiscountTotal = new Money(basket.DiscountTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+            basketModel.SubTotal = new Money(basket.SubTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+            basketModel.OrderTotal = new Money(basket.OrderTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+            basketModel.TaxTotal = new Money(basket.TaxTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+            basketModel.ShippingTotal = new Money(basket.ShippingTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+            basketModel.PaymentTotal = new Money(basket.PaymentTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+
+            return basketModel;
+        }
+
+        [HttpPost]
+        public ActionResult Index(bool checkout)
+        {
+            TransactionLibrary.RequestPayments();
+
+            return View("/Views/Complete.cshtml");
+        }
+    }
 }
