bbfa250e4bbfa102fd9ea253e90e3a4c389dd173
MSK <MSK@localhost>
2016-08-11 10:33:40 +0200

    Added models views and controllers for MVC example.

---
+++ src/MyUCommerceApp.Website/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UCommerce.Api;
+using UCommerce.EntitiesV2;
+using UCommerce.Extensions;
+using UCommerce.MasterClass.Website.Models;
+using UCommerce.Runtime;
+
+namespace UCommerce.MasterClass.Website.Controllers
+{
+	public class ProductController : System.Web.Mvc.Controller
+	{
+		public ActionResult Index()
+		{
+			ProductViewModel productModel = new ProductViewModel();
+
+			return View("/views/product.cshtml", productModel);
+		}
+
+		private IList<ProductViewModel> MapVariants(ICollection<UCommerce.EntitiesV2.Product> productsToMap)
+		{
+			var productModels = new List<ProductViewModel>();
+
+			return productModels;
+		}
+
+		[HttpPost]
+		public ActionResult Index(AddToBasketViewModel model)
+		{
+			return Index();
+		}
+	}
+}
---
+++ src/MyUCommerceApp.Website/Models/ShippingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UCommerce.MasterClass.Website.Models
+{
+	public class ShippingViewModel
+	{
+		public ShippingViewModel()
+		{
+			AvailableShippingMethods = new List<SelectListItem>();
+		}
+		public IList<SelectListItem> AvailableShippingMethods { get; set; }
+
+		public int SelectedShippingMethodId { get; set; }
+	}
+}
