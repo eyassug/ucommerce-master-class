4b25e076f6758047fa8b7f459bf1ca8cee0b7546
MSK <MSK@localhost>
2016-08-11 14:21:35 +0200

    Added basket, category and product.

--- src/MyUCommerceApp.Website/Controllers/MasterClassCategoryController.cs
+++ src/MyUCommerceApp.Website/Controllers/MasterClassCategoryController.cs
@@ -12,18 +12,40 @@ namespace MyUCommerceApp.Website.Controllers
 {
 	public class MasterClassCategoryController : Umbraco.Web.Mvc.RenderMvcController
 	{
-		public ActionResult Index()
-		{
-			var categoryViewModel = new CategoryViewModel();
+        public ActionResult Index()
+        {
+            var categoryViewModel = new CategoryViewModel();
 
-			return View("/views/category.cshtml",categoryViewModel);
-		}
+            var currentCategory = UCommerce.Runtime.SiteContext.Current.CatalogContext.CurrentCategory;
 
-		private IList<ProductViewModel> MapProducts(ICollection<UCommerce.EntitiesV2.Product> productsInCategory)
-		{
-			IList<ProductViewModel> productViews = new List<ProductViewModel>();
+            categoryViewModel.Name = currentCategory.DisplayName();
+            categoryViewModel.Description = currentCategory.Description();
 
-			return productViews;
-		}
-	}
+            var productsInCategory = UCommerce.Api.CatalogLibrary.GetProducts(currentCategory);
+
+            categoryViewModel.Products = MapProducts(productsInCategory);
+
+            return View("/views/category.cshtml", categoryViewModel);
+        }
+
+        private IList<ProductViewModel> MapProducts(ICollection<Product> productsInCategory)
+        {
+            IList<ProductViewModel> productViews = new List<ProductViewModel>();
+
+            foreach (UCommerce.EntitiesV2.Product product in productsInCategory)
+            {
+                var productViewModel = new ProductViewModel();
+
+                productViewModel.Sku = product.Sku;
+                productViewModel.Name = product.DisplayName();
+                productViewModel.Url = "/product?product=" + product.ProductId;
+
+                productViewModel.PriceCalculation = UCommerce.Api.CatalogLibrary.CalculatePrice(product);
+
+                productViews.Add(productViewModel);
+            }
+
+            return productViews;
+        }
+    }
 }
