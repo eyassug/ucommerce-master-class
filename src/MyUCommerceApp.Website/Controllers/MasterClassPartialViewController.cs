53e41c3248ba05f65b696f64e0e0418b5e10d450
MSK <MSK@localhost>
2016-08-22 15:44:52 +0200

    Category navigation.

--- src/MyUCommerceApp.Website/Controllers/MasterClassPartialViewController.cs
+++ src/MyUCommerceApp.Website/Controllers/MasterClassPartialViewController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CookComputing.Blogger;
 using MyUCommerceApp.Website.Models;
-
+using UCommerce.Api;
 namespace MyUCommerceApp.Website.Controllers
 {
 	public class MasterClassPartialViewController : Umbraco.Web.Mvc.SurfaceController
@@ -13,12 +14,25 @@ namespace MyUCommerceApp.Website.Controllers
         {
             var categoryNavigation = new CategoryNavigationViewModel();
 
+            categoryNavigation.Categories = MapCategories(UCommerce.Api.CatalogLibrary.GetRootCategories());
+
             return View("/views/PartialViews/CategoryNavigation.cshtml", categoryNavigation);
         }
 
         private IList<CategoryViewModel> MapCategories(ICollection<UCommerce.EntitiesV2.Category> categoriesToMap)
         {
             var categoriesToReturn = new List<CategoryViewModel>();
+
+            foreach (var category in categoriesToMap)
+            {
+                var categoryViewModel = new CategoryViewModel();
+
+                categoryViewModel.Name = category.Name;
+
+                categoryViewModel.Categories = MapCategories(UCommerce.Api.CatalogLibrary.GetCategories(category));
+
+                categoriesToReturn.Add(categoryViewModel);
+            }
 
             return categoriesToReturn;
         }
