440e46cb1262e198b666f538652bb22e33531f50
MSK <MSK@localhost>
2016-08-23 13:52:30 +0200

    Product page.

--- src/MyUCommerceApp.Website/Controllers/MasterClassProductController.cs
+++ src/MyUCommerceApp.Website/Controllers/MasterClassProductController.cs
@@ -3,30 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UCommerce.Api;
 using UCommerce.EntitiesV2;
+using UCommerce.Extensions;
 using MyUCommerceApp.Website.Models;
 
 namespace MyUCommerceApp.Website.Controllers
 {
-	public class MasterClassProductController : Umbraco.Web.Mvc.RenderMvcController
+    public class MasterClassProductController : Umbraco.Web.Mvc.RenderMvcController
     {
-		public ActionResult Index()
-		{
-			ProductViewModel productModel = new ProductViewModel();
+        public ActionResult Index()
+        {
+            var currentProduct = UCommerce.Runtime.SiteContext.Current.CatalogContext.CurrentProduct;
+
+            ProductViewModel productModel = new ProductViewModel();
+
+            productModel.PriceCalculation = UCommerce.Api.CatalogLibrary.CalculatePrice(currentProduct);
+            productModel.Sku = currentProduct.Sku;
+            productModel.Name = currentProduct.DisplayName();
+            productModel.LongDescription = currentProduct.LongDescription();
+            productModel.Variants = MapVariants(currentProduct.Variants);
+            productModel.IsVariant = currentProduct.ProductDefinition.IsProductFamily();
 
             return View("/views/product.cshtml", productModel);
-		}
+        }
 
-		private IList<ProductViewModel> MapVariants(ICollection<Product> variants)
-		{
-			var variantModels = new List<ProductViewModel>();
+        private IList<ProductViewModel> MapVariants(ICollection<Product> variants)
+        {
+            var variantModels = new List<ProductViewModel>();
 
-			return variantModels;
-		}
+            foreach (var currentProduct in variants)
+            {
+                ProductViewModel productModel = new ProductViewModel();
 
-		[HttpPost]
-		public ActionResult Index(AddToBasketViewModel model)
-		{
+                productModel.Sku = currentProduct.Sku;
+                productModel.VariantSku = currentProduct.VariantSku;
+                productModel.Name = currentProduct.DisplayName();
+                productModel.LongDescription = currentProduct.LongDescription();
+                productModel.Variants = MapVariants(currentProduct.Variants);
+
+                variantModels.Add(productModel);
+            }
+
+            return variantModels;
+        }
+
+        [HttpPost]
+        public ActionResult Index(AddToBasketViewModel model)
+        {
             return Index();
         }
     }
