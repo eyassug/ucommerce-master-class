d2526eef146dcc4f302189890700eac789d91c79
MSK <MSK@localhost>
2016-08-24 17:31:51 +0200

    Querying.

--- src/MyUCommerceApp.Runner/Program.cs
+++ src/MyUCommerceApp.Runner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using MyUCommerceApp.BusinessLogic.Queries;
 using NHibernate.Linq;
@@ -11,90 +12,59 @@ namespace MyUCommerceApp.Integration
 	{
 		static void Main(string[] args)
 		{
+            //            IRepository<PurchaseOrder> repository = ObjectFactory.Instance.Resolve<IRepository<PurchaseOrder>>();
+            //            ISessionProvider sessionProvider = ObjectFactory.Instance.Resolve<ISessionProvider>();
+            //
+            //            IList<PurchaseOrder> 
+            //                ordersByCertainDateFromStaticlayer = PurchaseOrder
+            //                    .All()
+            //                    .Where(x => x.CompletedDate > new DateTime(2009, 1, 1))
+            //                    .ToList();
+            //            
+            //            IList<PurchaseOrder> orderFromRepo = repository
+            //                    .Select(x => x.CompletedDate > new DateTime(2009, 1, 1))
+            //                    .ToList();
+            //
+            //            var ordersFromSessionProvider = sessionProvider.GetSession()
+            //		            .Query<UCommerce.EntitiesV2.PurchaseOrder>()
+            //		            .Where(x => x.CompletedDate > new DateTime(2009, 1, 1))
+            //		            .ToList();
+            //            IRepository<Product> productRepository = ObjectFactory.Instance.Resolve<IRepository<Product>>();
+            //            var products = productRepository.Select(new ProductsQueryByProperties("ShowOnHomepage", "true")).ToList();
 
-			//var products = session.CreateQuery(_productIds, @"
-			//	select p from Product p
-			//	where p.ProductId in ({0})")
-			//.SetCacheable(_cachable)
-			//.SetCacheRegion(cacheRegion)
-			//.SetTimeout(TimeoutPeriod)
-			//.Future<Product>();
+            //		    IList<PurchaseOrder> orders;
+            //		    var sessionProvider = ObjectFactory.Instance.Resolve<ISessionProvider>();
 
-			//session.CreateQuery(@"
-			//	select p from Product p
-			//	left outer join fetch p.ProductProperties pp
-			//	where p.ProductId in ({0})")
-			//.SetCacheable(_cachable)
-			//.SetCacheRegion(cacheRegion)
-			//.SetTimeout(TimeoutPeriod)
-			//.Future<Product>();
+            //using statements in the website is bad!!
+            //            using (var session = sessionProvider.GetSession())
+            //		    {
+            //1 query to load up the orders from the database.
+            //N = number of orders in the collection
+            //N+1 = 1 query to load up data + 1 query per relation we access.
 
+            //Fetch prevents N+1 problems!!!!
+            //		        orders = session.Query<PurchaseOrder>().Fetch(order => order.Customer).ToList();
+            //                foreach (var purchaseOrder in orders)
+            //                {
+            //                    if (purchaseOrder.Customer != null)
+            //                    {
+            //                        Console.WriteLine(purchaseOrder.Customer.FirstName);
+            //                        Console.WriteLine(purchaseOrder.BillingAddress.CompanyName);
+            //                    }
+            //                }
+            //            }
 
+   		    var sessionProvider = ObjectFactory.Instance.Resolve<ISessionProvider>();
+		    sessionProvider
+                .GetSession()
+                .Query<Product>()
+                .Where(x => x.ProductId == 105)
+                .FetchMany(x => x.Variants)
+                .FetchMany(x => x.CategoryProductRelations)
+                .FetchMany(x => x.ProductRelations)
+                .ToList();
+
+		    //Console.ReadLine();
 		}
-
-		private static void Fetch_While_Avoiding_Large_Cartesian_Product()
-		{
-			var orderRepository = ObjectFactory.Instance.Resolve<IRepository<PurchaseOrder>>();
-
-			var orderlineRepository = ObjectFactory.Instance.Resolve<IRepository<OrderLine>>();
-
-			var orderQuery = orderRepository
-				.Select()
-				.Where(x => x.BillingAddress != null).ToFuture();
-
-			orderlineRepository.Select().Where(x => x.PurchaseOrder.BillingAddress != null).ToFuture();
-
-			foreach (var order in orderQuery.ToList())
-			{
-				Console.WriteLine("{0} {1}", order.BillingAddress.FirstName, order.BillingAddress.LastName);
-
-				Console.WriteLine(order.OrderLines.First().Sku);
-			}
-
-			Console.ReadLine();
-		}
-
-		private static void Join_Product_And_OrderLine()
-		{
-			var productRepo = ObjectFactory.Instance.Resolve<IRepository<Product>>();
-			var orderlineRepo = ObjectFactory.Instance.Resolve<IRepository<OrderLine>>();
-
-			var query = from orderline in orderlineRepo.Select()
-				join product in productRepo.Select() on
-					new {orderline.Sku, orderline.VariantSku} equals new {product.Sku, product.VariantSku}
-				select new {OrderLine = orderline, Product = product};
-
-			var orderlinesWithProducts = query.ToList();
-		}
-
-		private static void Query_By_Custom_Product_Properties()
-		{
-			var productRepo = ObjectFactory.Instance.Resolve<IRepository<Product>>();
-			var query = productRepo.Select().Where(
-				x => x.ProductProperties.Any(
-					y => y.ProductDefinitionField.Name == "ShowOnHomepage" && y.Value == "True"));
-
-			var productsOnFrontpage = query.ToList();
-		}
-
-		private static void Query_Orders_By_Date()
-		{
-			var orderRepository = ObjectFactory.Instance.Resolve<IRepository<PurchaseOrder>>();
-			IQueryable<PurchaseOrder> query = orderRepository.Select();
-
-			var orders = query.Where(x => x.CompletedDate >= new DateTime(2016, 6, 7))
-				.ToList();
-
-			foreach (var order in orders)
-			{
-				var orderTotal = order.OrderTotal;
-			}
-		}
-
-		private static void Canned_Query_Example()
-		{
-			var order = ObjectFactory.Instance.Resolve<IRepository<PurchaseOrder>>()
-				.Select(new LatestOrderQuery()).First();
-		}
-	}
+    }
 }
---
+++ src/MyUCommerceApp/Queries/ProductsQueryByProperties.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using NHibernate.Linq;
+using UCommerce.EntitiesV2;
+using UCommerce.EntitiesV2.Queries;
+
+namespace MyUCommerceApp.BusinessLogic.Queries
+{
+    public class ProductsQueryByProperties : UCommerce.EntitiesV2.Queries.ICannedQuery<UCommerce.EntitiesV2.Product>
+    {
+        private readonly string _fieldName;
+        private readonly string _fieldValue;
+
+        public ProductsQueryByProperties(string fieldName, string fieldValue)
+        {
+            _fieldName = fieldName;
+            _fieldValue = fieldValue;
+        }
+        public IEnumerable<Product> Execute(NHibernate.ISession session)
+        {
+            return session
+                .Query<Product>()
+                .Where(
+                    product => product.ProductProperties.Any(property => property.Value == _fieldValue &&
+                                         property.ProductDefinitionField.Name == _fieldName)).ToList();
+        }
+    }
+}
