cd7759debc6c776128cf9255154515935a8ecde1
MSK <MSK@localhost>
2016-08-25 17:18:14 +0200

    Day4.

---
+++ src/MyUCommerceApp/Payments/MyCustomPaymentGateWayImplementaion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UCommerce.EntitiesV2;
+using UCommerce.Transactions.Payments;
+
+namespace MyUCommerceApp.BusinessLogic.Payments
+{
+    public class MyCustomPaymentGateWayImplementaion : ExternalPaymentMethodService
+    {
+        public override Payment RequestPayment(PaymentRequest paymentRequest)
+        {
+            return base.RequestPayment(paymentRequest);
+        }
+
+        public override string RenderPage(PaymentRequest paymentRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ProcessCallback(Payment payment)
+        {
+            //if the user is redirected then you need to redirect the user in here.
+            //If this is a server to server request, don't do anything
+            throw new NotImplementedException();
+        }
+
+        protected override bool CancelPaymentInternal(Payment payment, out string status)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool AcquirePaymentInternal(Payment payment, out string status)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool RefundPaymentInternal(Payment payment, out string status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MyUCommerceApp/Tax/ExtendedTaxService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UCommerce;
+using UCommerce.Catalog;
+using UCommerce.EntitiesV2;
+using UCommerce.Security;
+
+namespace MyUCommerceApp.BusinessLogic.Tax
+{
+    public class ExtendedTaxService : TaxService
+    {
+        private readonly IRepository<PriceGroup> _priceGroupRepository;
+
+        public ExtendedTaxService(IRepository<PriceGroup> priceGroupRepository)
+        {
+            _priceGroupRepository = priceGroupRepository;
+        }
+
+        public override Money CalculateTax(Product product, PriceGroup priceGroup, Money unitPrice)
+        {
+            var priceGroupProperty = product["PriceGroup"];
+
+            if (priceGroupProperty == null && product.ParentProduct != null)
+            {
+                priceGroupProperty = product.ParentProduct["PriceGroup"];
+            }
+            
+            if (priceGroupProperty == null || priceGroupProperty.GetValue() == null)
+                return base.CalculateTax(product, priceGroup, unitPrice);
+
+            int priceGroupId;
+            if (int.TryParse(priceGroupProperty.GetValue().ToString(), out priceGroupId))
+            {
+                var overridenPriceGroup = _priceGroupRepository.Select().Where(x => x.PriceGroupId == priceGroupId).FirstOrDefault();
+                if (overridenPriceGroup != null)
+                {
+                    priceGroup = overridenPriceGroup;
+                }
+            }
+            
+            return base.CalculateTax(product, priceGroup, unitPrice);
+        }
+    }
+}
