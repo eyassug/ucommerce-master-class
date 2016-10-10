731c1c6d5f3df431cc3126d38265b7bfc3e7497c
Mads Due <Mads Due@localhost>
2016-10-10 17:04:35 +0200

    Day 1: Browsing.

--- src/MyUCommerceApp.Website/Controllers/MasterClassCategoryController.cs
+++ src/MyUCommerceApp.Website/Controllers/MasterClassCategoryController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using UCommerce.EntitiesV2;
 using MyUCommerceApp.Website.Models;
+using UCommerce.EntitiesV2;
+using UCommerce.Runtime;
+using UCommerce.Extensions;
 
 namespace MyUCommerceApp.Website.Controllers
 {
@@ -14,12 +17,35 @@ namespace MyUCommerceApp.Website.Controllers
         {
             var categoryViewModel = new CategoryViewModel();
 
+	        var currentCategory = 
+				UCommerce.Runtime.SiteContext.Current.CatalogContext.CurrentCategory;
+
+	        categoryViewModel.Name = currentCategory.DisplayName();
+	        categoryViewModel.Description = currentCategory.Description();
+
+	        var productsInCategory = UCommerce.Api.CatalogLibrary.GetProducts(currentCategory);
+	        categoryViewModel.Products = MapProducts(productsInCategory);
+
             return View("/views/category.cshtml", categoryViewModel);
         }
 
-        private IList<ProductViewModel> MapProducts(ICollection<Product> productsInCategory)
+        private IList<ProductViewModel> MapProducts(ICollection<UCommerce.EntitiesV2.Product> productsInCategory)
         {
             IList<ProductViewModel> productViews = new List<ProductViewModel>();
+
+	        foreach (var product in productsInCategory)
+	        {
+		        var productViewModel = new ProductViewModel();
+
+		        productViewModel.Sku = product.Sku;
+		        productViewModel.Name = product.DisplayName();
+
+		        productViewModel.Url = "/product?product=" + product.ProductId;
+
+		        productViewModel.PriceCalculation = UCommerce.Api.CatalogLibrary.CalculatePrice(product);
+	        
+				productViews.Add(productViewModel);
+			}
 
             return productViews;
         }
