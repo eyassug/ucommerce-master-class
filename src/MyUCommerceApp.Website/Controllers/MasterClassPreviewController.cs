07fa946a1a8e28fadcfdcbc97c308cae3055a13b
Mads Due <Mads Due@localhost>
2016-10-12 11:47:37 +0200

    Day 2: Transaction.

--- src/MyUCommerceApp.Website/Controllers/MasterClassPreviewController.cs
+++ src/MyUCommerceApp.Website/Controllers/MasterClassPreviewController.cs
@@ -7,6 +7,8 @@ using UCommerce.Api;
 using UCommerce.EntitiesV2;
 using UCommerce;
 using MyUCommerceApp.Website.Models;
+using UCommerce.Api;
+using UCommerce.EntitiesV2.Queries.Catalog;
 
 namespace MyUCommerceApp.Website.Controllers
 {
@@ -19,8 +21,38 @@ namespace MyUCommerceApp.Website.Controllers
             model.BillingAddress = MapOrderAddress(TransactionLibrary.GetBillingInformation());
             model.ShippingAddress = MapOrderAddress(TransactionLibrary.GetShippingInformation());
 
+	        var basket = TransactionLibrary.GetBasket().PurchaseOrder;
+	        var billingCurrency = basket.BillingCurrency;
+
+	        foreach (var orderLine in basket.OrderLines)
+	        {
+		        var orderLineViewModel = new OrderlineViewModel();
+
+		        orderLineViewModel.Sku = orderLine.Sku;
+		        orderLineViewModel.VariantSku = orderLine.VariantSku;
+		        orderLineViewModel.ProductName = orderLine.ProductName;
+		        orderLineViewModel.Quantity = orderLine.Quantity;
+
+				orderLineViewModel.Total = new Money(orderLine.Total.GetValueOrDefault(), billingCurrency).ToString();
+
+				model.OrderLines.Add(orderLineViewModel);
+	        }
+
+			model.SubTotal = new Money(basket.SubTotal.GetValueOrDefault(), billingCurrency).ToString();
+	        model.TaxTotal = GetMoneyFormat(basket.TaxTotal.GetValueOrDefault(), billingCurrency);
+	        model.DiscountTotal = GetMoneyFormat(basket.DiscountTotal.GetValueOrDefault(), billingCurrency);
+	        model.ShippingTotal = GetMoneyFormat(basket.ShippingTotal.GetValueOrDefault(), billingCurrency);
+	        model.PaymentTotal = GetMoneyFormat(basket.PaymentTotal.GetValueOrDefault(), billingCurrency);
+	        model.OrderTotal = GetMoneyFormat(basket.OrderTotal.GetValueOrDefault(), billingCurrency);
+
+
             return View("/Views/preview.cshtml", model);
         }
+
+		private string GetMoneyFormat(decimal? value, Currency currency)
+		{
+			return new Money(value.GetValueOrDefault(), currency).ToString();
+		}
 
         private AddressViewModel MapOrderAddress(OrderAddress orderAddress)
         {
@@ -46,13 +78,15 @@ namespace MyUCommerceApp.Website.Controllers
         private PurchaseOrderViewModel MapOrder()
         {
             var basketModel = new PurchaseOrderViewModel();
-
+			var order = TransactionLibrary.GetPurchaseOrder(Request.QueryString["OrderGuid"])
             return basketModel;
         }
 
         [HttpPost]
         public ActionResult Index(bool checkout)
         {
+			TransactionLibrary.RequestPayments();
+
             return View("/Views/Complete.cshtml");
         }
     }
--- src/MyUCommerceApp.Website/Controllers/MasterClassShippingController.cs
+++ src/MyUCommerceApp.Website/Controllers/MasterClassShippingController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyUCommerceApp.Website.Models;
+using UCommerce.EntitiesV2;
+using UCommerce.Api;
 
 namespace MyUCommerceApp.Website.Controllers
 {
@@ -13,12 +15,42 @@ namespace MyUCommerceApp.Website.Controllers
         {
             var shippingModel = new ShippingViewModel();
 
+	        var shippingInformation = TransactionLibrary.GetShippingInformation();
+
+	        ICollection<ShippingMethod> availableShippingMethods =
+		        UCommerce.Api.TransactionLibrary.GetShippingMethods(shippingInformation.Country);
+
+	        ShippingMethod selectedShippingMethod = TransactionLibrary.GetShippingMethod();
+	        int selectedShippingMethodId = -1;
+	        if (selectedShippingMethod != null)
+	        {
+		        selectedShippingMethodId = selectedShippingMethod.ShippingMethodId;
+	        }
+
+
+	        foreach (var availableShippingMethod in availableShippingMethods)
+	        {
+				shippingModel.AvailableShippingMethods.Add(new SelectListItem()
+				{
+					Selected = selectedShippingMethodId == availableShippingMethod.ShippingMethodId,
+					Text = availableShippingMethod.Name,
+					Value = availableShippingMethod.ShippingMethodId.ToString()
+				});
+	        }
+
             return View("/Views/Shipping.cshtml", shippingModel);
         }
 
         [HttpPost]
         public ActionResult Index(ShippingViewModel shipping)
         {
+			TransactionLibrary.CreateShipment(
+				shippingMethodId: shipping.SelectedShippingMethodId,
+				addressName: null,
+				overwriteExisting: true);
+
+			TransactionLibrary.ExecuteBasketPipeline();
+
             return Redirect("/payment");
 		}
 	}
