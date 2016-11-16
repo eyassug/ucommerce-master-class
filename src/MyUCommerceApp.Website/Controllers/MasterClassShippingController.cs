4aa75bb1eb61ef5db2242139f7eb389a7523d236
Mads Due <Mads Due@localhost>
2016-11-16 13:54:30 +0100

    Day 2: Checkout flow.

--- src/MyUCommerceApp.Website/Controllers/MasterClassShippingController.cs
+++ src/MyUCommerceApp.Website/Controllers/MasterClassShippingController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyUCommerceApp.Website.Models;
+using UCommerce.Api;
+using UCommerce.EntitiesV2;
 
 namespace MyUCommerceApp.Website.Controllers
 {
@@ -13,12 +15,41 @@ namespace MyUCommerceApp.Website.Controllers
         {
             var shippingModel = new ShippingViewModel();
 
+	        UCommerce.EntitiesV2.OrderAddress shippingInfomation = TransactionLibrary.GetShippingInformation();
+
+	        ICollection<ShippingMethod> availableShippingMethods =
+		        TransactionLibrary.GetShippingMethods(shippingInfomation.Country);
+
+	        ShippingMethod selectedShippingMethod = UCommerce.Api.TransactionLibrary.GetShippingMethod();
+	        int selectedShippingMethodId = -1;
+	        if (selectedShippingMethod != null)
+	        {
+		        selectedShippingMethodId = selectedShippingMethod.ShippingMethodId;
+	        }
+
+	        foreach (var availableShippingMethod in availableShippingMethods)
+	        {
+		        shippingModel.AvailableShippingMethods.Add(new SelectListItem()
+		        {
+			        Selected = selectedShippingMethodId == availableShippingMethod.ShippingMethodId,
+					Text = availableShippingMethod.Name,
+					Value = availableShippingMethod.ShippingMethodId.ToString()
+		        });
+	        }
+
             return View("/Views/mc/Shipping.cshtml", shippingModel);
         }
 
         [HttpPost]
         public ActionResult Index(ShippingViewModel shipping)
         {
+			TransactionLibrary.CreateShipment(
+				shippingMethodId: shipping.SelectedShippingMethodId,
+				addressName: null,
+				overwriteExisting: true);
+
+			TransactionLibrary.ExecuteBasketPipeline();
+
             return Redirect("/payment");
 		}
 	}
