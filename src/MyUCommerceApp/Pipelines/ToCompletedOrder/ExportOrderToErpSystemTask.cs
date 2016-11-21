ef0c3a0a8e82aee46dd95a5bf3562541b3be7762
Mads Due <Mads Due@localhost>
2016-11-21 13:05:35 +0100

    Day 4: Deep Dive part 2

---
+++ src/MyUCommerceApp/Pipelines/ToCompletedOrder/ExportOrderToErpSystemTask.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UCommerce;
+using UCommerce.EntitiesV2;
+using UCommerce.Pipelines;
+
+namespace MyUCommerceApp.BusinessLogic.Pipelines.ToCompletedOrder
+{
+	public class ExportOrderToErpSystemTask : UCommerce.Pipelines.IPipelineTask<UCommerce.EntitiesV2.PurchaseOrder>
+	{
+		public PipelineExecutionResult Execute(PurchaseOrder subject)
+		{
+			string orderInformation = string.Format("{0}: {1} customer: {2}",
+				subject.OrderNumber,
+				new Money(subject.OrderTotal.GetValueOrDefault(), subject.BillingCurrency),
+				subject.Customer.EmailAddress);
+
+			File.AppendAllText("C:\\ERP\\ERP.txt", orderInformation);
+
+			return PipelineExecutionResult.Success;
+		}
+	}
+}
