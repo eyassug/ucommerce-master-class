d89ec1b8ab5554f1e46945f29dc864b5b28abb4d
Mads Due <Mads Due@localhost>
2017-03-14 18:49:30 +0100

    Day 2. Checkout

--- src/MyUCommerceApp.Website/Controllers/MasterClassShippingController.cs
+++ src/MyUCommerceApp.Website/Controllers/MasterClassShippingController.cs
@@ -1,5 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using MyUCommerceApp.Website.Models;
+using UCommerce.Api;
+using UCommerce.EntitiesV2;
 
 namespace MyUCommerceApp.Website.Controllers
 {
@@ -10,12 +13,39 @@ namespace MyUCommerceApp.Website.Controllers
         {
             var shippingModel = new ShippingViewModel();
 
+            OrderAddress shippingInformation = TransactionLibrary.GetShippingInformation();
+
+            ICollection<ShippingMethod> availableShippingMethods =
+                TransactionLibrary.GetShippingMethods(shippingInformation.Country);
+
+            ShippingMethod selectedShippingMethod = UCommerce.Api.TransactionLibrary.GetShippingMethod();
+            int selectedShippingMethodId = -1;
+            if (selectedShippingMethod != null)
+            {
+                selectedShippingMethodId = selectedShippingMethod.ShippingMethodId;
+            }
+
+            foreach (var availableShippingMethod in availableShippingMethods)
+            {
+                shippingModel.AvailableShippingMethods.Add(new SelectListItem()
+                {
+                    Selected = selectedShippingMethodId == availableShippingMethod.ShippingMethodId,
+                    Text =  availableShippingMethod.Name,
+                    Value = availableShippingMethod.ShippingMethodId.ToString()
+                });
+            }
             return View("/Views/mc/Shipping.cshtml", shippingModel);
         }
 
         [HttpPost]
         public ActionResult Index(ShippingViewModel shipping)
         {
+            TransactionLibrary.CreateShipment(
+                shippingMethodId: shipping.SelectedShippingMethodId,
+                addressName:null, 
+                overwriteExisting:true);
+
+            TransactionLibrary.ExecuteBasketPipeline();
             return Redirect("/payment");
 		}
 	}
