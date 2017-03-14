c2cbf643a6b576a7241f0cfa7ca3d1b389670f95
Mads Due <Mads Due@localhost>
2017-03-14 18:49:30 +0100

    Day 2. Checkout

--- src/MyUCommerceApp.Website/Controllers/MasterClassBasketController.cs
+++ src/MyUCommerceApp.Website/Controllers/MasterClassBasketController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Web.Mvc;
 using MyUCommerceApp.Website.Models;
+using UCommerce;
+using UCommerce.Api;
+using UCommerce.EntitiesV2;
 
 namespace MyUCommerceApp.Website.Controllers
 {
@@ -10,12 +13,47 @@ namespace MyUCommerceApp.Website.Controllers
         {
             var basketModel = new PurchaseOrderViewModel();
 
+            UCommerce.EntitiesV2.PurchaseOrder basket = UCommerce.Api.TransactionLibrary.GetBasket(false).PurchaseOrder;
+
+            UCommerce.EntitiesV2.Currency billingCurrency = basket.BillingCurrency;
+
+            basketModel.OrderTotal = new Money(basket.OrderTotal.GetValueOrDefault(), billingCurrency).ToString();
+
+            foreach (var orderLine in basket.OrderLines)
+            {
+                var orderLineViewModel = new  OrderlineViewModel();
+
+                orderLineViewModel.Quantity = orderLine.Quantity;
+                orderLineViewModel.ProductName = orderLine.ProductName;
+                orderLineViewModel.Sku = orderLine.Sku;
+                orderLineViewModel.VariantSku = orderLine.VariantSku;
+                orderLineViewModel.Total = new Money(orderLine.Total.GetValueOrDefault(), billingCurrency).ToString();
+                orderLineViewModel.OrderLineId = orderLine.OrderLineId;
+
+                basketModel.OrderLines.Add(orderLineViewModel);
+            }
+
             return View("/Views/mc/Basket.cshtml", basketModel);
         }
 
         [HttpPost]
         public ActionResult Index(PurchaseOrderViewModel model)
         {
+            foreach (var orderLine in model.OrderLines)
+            {
+                int newQuantity = orderLine.Quantity;
+                int orderLineId = orderLine.OrderLineId;
+
+                if (model.RemoveOrderlineId == orderLineId)
+                {
+                    newQuantity = 0;
+                }
+
+                UCommerce.Api.TransactionLibrary.UpdateLineItem(orderLineId, newQuantity);
+            }
+
+            UCommerce.Api.TransactionLibrary.ExecuteBasketPipeline();
+
             return Redirect(this.CurrentPage.Url);
         }
     }
--- src/MyUCommerceApp.Website/Controllers/MasterClassPreviewController.cs
+++ src/MyUCommerceApp.Website/Controllers/MasterClassPreviewController.cs
@@ -2,6 +2,7 @@
 using UCommerce.Api;
 using UCommerce.EntitiesV2;
 using MyUCommerceApp.Website.Models;
+using UCommerce;
 
 namespace MyUCommerceApp.Website.Controllers
 {
@@ -15,7 +16,37 @@ namespace MyUCommerceApp.Website.Controllers
             model.BillingAddress = MapOrderAddress(TransactionLibrary.GetBillingInformation());
             model.ShippingAddress = MapOrderAddress(TransactionLibrary.GetShippingInformation());
 
+            var basket = UCommerce.Api.TransactionLibrary.GetBasket(false).PurchaseOrder;
+            var billingCurrency = basket.BillingCurrency;
+
+            foreach (var basketOrderLine in basket.OrderLines)
+            {
+                var orderLineViewModel = new OrderlineViewModel();
+
+                orderLineViewModel.Sku = basketOrderLine.Sku;
+                orderLineViewModel.VariantSku = basketOrderLine.VariantSku;
+                orderLineViewModel.ProductName = basketOrderLine.ProductName;
+                orderLineViewModel.Quantity = basketOrderLine.Quantity;
+
+                orderLineViewModel.Total = new Money(basketOrderLine.Total.GetValueOrDefault(), billingCurrency).ToString();
+                
+                model.OrderLines.Add(orderLineViewModel);
+            }
+
+            model.SubTotal = new Money(basket.SubTotal.GetValueOrDefault(), billingCurrency).ToString();
+            model.TaxTotal = new Money(basket.TaxTotal.GetValueOrDefault(), billingCurrency).ToString();
+            model.DiscountTotal = new Money(basket.DiscountTotal.GetValueOrDefault(), billingCurrency).ToString();
+
+            model.ShippingTotal = GetMoneyFormat(basket.ShippingTotal, billingCurrency);
+            model.PaymentTotal = GetMoneyFormat(basket.PaymentTotal, billingCurrency);
+            model.OrderTotal = GetMoneyFormat(basket.OrderTotal, billingCurrency);
+
             return View("/Views/mc/preview.cshtml", model);
+        }
+
+        private string GetMoneyFormat(decimal? value, Currency currency)
+        {
+            return new Money(value.GetValueOrDefault(), currency).ToString();
         }
 
         private AddressViewModel MapOrderAddress(OrderAddress orderAddress)
@@ -49,6 +80,8 @@ namespace MyUCommerceApp.Website.Controllers
         [HttpPost]
         public ActionResult Index(bool checkout)
         {
+            TransactionLibrary.RequestPayments();
+
             return View("/Views/mc/Complete.cshtml");
         }
     }
